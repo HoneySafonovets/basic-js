e123d25bab9aabdf7c41e64c9ae6eca2823ed0ba
HoneySafonovets <kfs.boi@yandex.ru>
2025-11-10 18:24:22 +0300

    feat: add function DNS-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,51 @@ const { NotImplementedError } = require('../lib');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  if (domains.length === 0) {
+    return {};
+  }
+  let resultObj = {};
+  let helpArrray = [];
+  let countAgain = '';
+
+  if (domains.length === 1) {
+    let array = domains[0].split('.').reverse();
+    array.forEach((e, index) => {
+      if (index === 0) {
+        resultObj[`.${e}`] = 1;
+        helpArrray.push(`.${e}`);
+      }
+      if (index >= 1) {
+        resultObj[`${helpArrray[index - 1]}.${e}`] = 1;
+      }
+    });
+  }
+  
+  if (domains.length === 2) {
+    domains.forEach((e) => {
+      if (countAgain.length < e.length) {
+        countAgain = e
+      }
+    });
+
+    let array = countAgain.split('.').reverse();
+    array.forEach((e, index) => {
+      if (index === 0) {
+        resultObj[`.${e}`] = 2;
+        helpArrray.push(`.${e}`);
+      }
+      if (index === 1) {
+        resultObj[`${helpArrray[index - 1]}.${e}`] = 2;
+        helpArrray.push(`.${e}`);
+      }
+      if (index === 2) {
+        resultObj[`${helpArrray[index - 2]}${helpArrray[index - 1]}.${e}`] = 1;
+      }
+    });
+  };
+  
+  return resultObj;
 }
 
 module.exports = {
