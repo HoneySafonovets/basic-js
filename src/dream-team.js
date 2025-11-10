559a3381ea38ea721663f793ca365d82205c87a2
HoneySafonovets <kfs.boi@yandex.ru>
2025-11-10 19:00:19 +0300

    feat: add function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,20 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let herlpArray = [];
+  
+  members.forEach((e) => {
+    if (typeof e === 'string') {
+      let whuiteSpace = e.replaceAll(' ', '')
+      herlpArray.push(whuiteSpace[0].toUpperCase());
+    }
+  });
+
+  return herlpArray.sort().join('');
 }
 
 module.exports = {
