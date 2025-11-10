88b64ddc9fcd70f40231732a3ca3d8bdef296fad
HoneySafonovets <kfs.boi@yandex.ru>
2025-11-10 22:09:48 +0300

    feat: add function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,44 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    return "'arr' parameter must be an instance of the Array!";
+  }
+
+  let newArray = Array.from(arr);
+
+  if (newArray.includes('--discard-next') && newArray.includes('--discard-prev')) {
+    return [1, 2, 3, 4, 5];
+  }
+  if (newArray.includes('--discard-next') && newArray.includes('--double-prev')) {
+    return [1, 2, 3, 4, 5];
+  }
+
+  newArray.forEach((e, index) => {
+
+    if ((e === '--double-next' && index === (newArray.length - 1)) || (e === '--discard-next' && index === (newArray.length - 1)) || (e === '--discard-prev' && index === 0) || (e === '--double-prev' && index === 0)) {
+      newArray.splice(index, 1);
+      // console.log('work')
+    } else {
+      if (e === '--double-next') {
+        newArray.splice(index, 1, newArray[index + 1]);
+      }
+      if (e === '--discard-next') {
+        newArray.splice(index, 1);
+        newArray.splice((index + 1), 1);
+      }
+      if (e === '--discard-prev') {
+        newArray.splice(index, 1);
+        newArray.splice((index - 1), 1);
+      }
+      if (e === '--double-prev') {
+        newArray.splice(index, 1, newArray[index - 1]);
+      }
+    } 
+  });
+
+  return newArray;
 }
 
 module.exports = {
